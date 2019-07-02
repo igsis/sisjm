3c43a7db84f6b04d3ab69a4d1e982251bb9ea237
mariacarolina1701 <51335300+mariacarolina1701@users.noreply.github.com>
2019-07-02 16:58:49 -0300

    Ajuste conexão

--- funcoes/funcoesConecta.php
+++ funcoes/funcoesConecta.php
@@ -31,6 +31,7 @@ function bancoPDO()
 	}
 }
 // Conexão com banco do CAPAC
+
 function bancoCapac()
 {
     $servidor = 'localhost';
@@ -41,14 +42,35 @@ function bancoCapac()
     mysqli_set_charset($con,"utf8");
     return $con;
 }
+// Conex�o de Banco com PDO
+function bancoPDOCapac()
+{
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db = 'capac';
+    $charset = 'utf8';
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset;";
+
+    try {
+        $conn = new PDO($dsn, $user, $pass);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $conn;
+    }
+    catch(PDOException $e)
+    {
+        echo "Erro " . $e->getMessage();
+    }
+}
 // Cria conexao ao banco de CEPs.
 function bancoMysqliCep()
 {
-	$servidor = 'localhost';
-	$usuario = 'root';
-	$senha = '';
-	$banco = 'cep';
-	$con = mysqli_connect($servidor,$usuario,$senha,$banco);
-	mysqli_set_charset($con,"utf8");
-	return $con;
+    $servidor = 'localhost';
+    $usuario = 'root';
+    $senha = '';
+    $banco = 'cep';
+    $con = mysqli_connect($servidor,$usuario,$senha,$banco);
+    mysqli_set_charset($con,"utf8");
+    return $con;
 }
